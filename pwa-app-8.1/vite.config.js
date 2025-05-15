fe9ce24eaf47d46a91a52de53fc8186666a1e739
Mllyszn <melvinlieuw121107@gmail.com>
2025-05-15 11:07:01 +0200

    npm run dev fix + tailwind install

--- pwa-app-8.1/vite.config.js
+++ pwa-app-8.1/vite.config.js
@@ -1,10 +1,14 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+
+  plugins: [
+     react(), VitePWA, tailwindcss(), 
+  ],
     strategies: 'injectManifest',
     srcDir: 'src',
     filename: 'sw.js',
@@ -33,5 +37,4 @@ export default defineConfig({
       suppressWarnings: true,
       type: 'module',
     },
-  })],
-})
+  })
